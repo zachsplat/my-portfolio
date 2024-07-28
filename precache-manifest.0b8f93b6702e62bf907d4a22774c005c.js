ba9aa2975cb620ec4d56fdbaf7618b24a4f6063c
Zachary Alexander <zalexander14@icloud.com>
2024-07-28 01:49:03 -0500

    Updates

---
+++ precache-manifest.0b8f93b6702e62bf907d4a22774c005c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7799670fc37cfbc4b698cddeb1652d",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "cf236721cf7a048d806c",
+    "url": "/my-portfolio/static/css/main.2d1573f7.chunk.css"
+  },
+  {
+    "revision": "95e1f0f803acc61e2952",
+    "url": "/my-portfolio/static/js/2.9f140c04.chunk.js"
+  },
+  {
+    "revision": "cf236721cf7a048d806c",
+    "url": "/my-portfolio/static/js/main.de3736c4.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%3Czachsplat%3E.github.io/my-portfolio/precache-manifest.877e68831880bc2c1abf77f50ddcd5b3.js"
+  "/my-portfolio/precache-manifest.0b8f93b6702e62bf907d4a22774c005c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%3Czachsplat%3E.github.io/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
