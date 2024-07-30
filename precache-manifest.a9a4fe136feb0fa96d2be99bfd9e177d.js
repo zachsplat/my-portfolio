55a24003ab90724ad708068def9a0b70c6d69ada
Zachary Alexander <zalexander14@icloud.com>
2024-07-30 14:56:53 -0500

    Updates

---
+++ precache-manifest.a9a4fe136feb0fa96d2be99bfd9e177d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947c452406ef05d9a05a125b00b0dd15",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "87454ef0bcb4f1412f11",
+    "url": "/my-portfolio/static/css/main.6942960f.chunk.css"
+  },
+  {
+    "revision": "1c7c974299eccf198426",
+    "url": "/my-portfolio/static/js/2.ca62a5b9.chunk.js"
+  },
+  {
+    "revision": "87454ef0bcb4f1412f11",
+    "url": "/my-portfolio/static/js/main.0fa34e21.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.de93f3393406bcd27830d3a75095b923.js"
+  "/my-portfolio/precache-manifest.a9a4fe136feb0fa96d2be99bfd9e177d.js"
 );
 
 self.addEventListener('message', (event) => {
